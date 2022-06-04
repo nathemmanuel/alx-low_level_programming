89b2ab24b7efd0be36c5454a8a6d0d22cf2a9d6e
Bello Emmanuel <nathemmanuel.03@gmail.com>
2022-06-04 16:14:26 +0100

    Add: Scripts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - Entry point for compiler
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
