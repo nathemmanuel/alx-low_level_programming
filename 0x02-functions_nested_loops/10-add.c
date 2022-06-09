3ba1e502ba0d69053c0b6bf536e831fb68c91c10
Bello Emmanuel <nathemmanuel.03@gmail.com>
2022-06-09 14:40:59 +0100

    Modify: Tasks 9 and 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -8,7 +8,7 @@
  * Return: The result of the addition
  */
 
-int add (int a, int b)
+int add(int a, int b)
 {
 	return (a + b);
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,11 +10,16 @@ void times_table(void)
 {
 	int i, j;
 
-	for (i = 0; i == 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j == 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			_putchar(i * j);
+			if ((i * j) > 9)
+			{
+				_putchar((i*j)/10);
+				_putchar((i*j) % 10);
+			}
 			_putchar(',');
 			_putchar(' ');
 		}
