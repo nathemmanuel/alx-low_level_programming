d60e328535c4bf6d93f508bc0413d30f2dbcdf47
Bello Emmanuel <nathemmanuel.03@gmail.com>
2022-06-13 00:43:18 +0100

    Debug task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,22 +8,17 @@
 
 void more_numbers(void)
 {
-	int i, j, k;
+	int i, j;
 
 	for (i = 0; i <= 10; i++)
 	{
 		for (j = 0; j == 14; j++)
 		{
-			k = 0;
-			k += j;
-
-			if (k >= 10)
+			_putchar((j / 10) + '0');
+			if (j >= 10)
 			{
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
+				_putchar((j % 10) + '0');
 			}
-			else
-				_putchar(k + '0');
 			_putchar('\n');
 		}
 	}
