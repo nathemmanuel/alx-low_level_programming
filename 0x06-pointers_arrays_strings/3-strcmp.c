c1938736f7ab7429dade206982df055986f231ae
Bello Emmanuel <nathemmanuel.03@gmail.com>
2022-06-16 13:21:28 +0100

    Debug

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,10 +14,11 @@ int _strcmp(char *s1, char *s2)
 	int i;
 
 	i = 0;
-	while (*(s2 + i) != '\0')
+	while (*(s2 + i) != '\0' && *(s1 + i) != '\0')
 	{
 		if (*(s2 + i) != *(s1 + i))
 			return (*(s1 + i) - *(s2 + i));
 		i++;
 	}
+	return (0);
 }
