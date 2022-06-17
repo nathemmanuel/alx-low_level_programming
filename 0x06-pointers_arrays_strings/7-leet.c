1ec80efb06aeeb675af551933a4260707fe55e7f
Bello Emmanuel <nathemmanuel.03@gmail.com>
2022-06-17 01:35:35 +0100

    Add: 7-leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,16 +2,31 @@
 
 /**
  * leet - replaces certain characters with digits
- * @c: String
+ * @string: String
  *
  * Return: c
  */
 
-char *leet(char *c)
+char *leet(char *string)
 {
-	int i;
+	int i, j;
+
+	char a[] = {'a', 'e', 'o', 't', 'l'};
+	char b[] = {'4', '3', '0', '7', '1'};
 
 	i = 0;
-	while (c[i] != '\0')
+	j = 0;
+	while (*(string + j) != '\0')
 	{
-		
+		while (i < 5)
+		{
+			if (*(a + i) || *(a + i) - 32)
+			{
+				*(a + i) = *(b + i);
+			}
+			i++;
+			j++;
+		}
+	}
+	return (string);
+}
