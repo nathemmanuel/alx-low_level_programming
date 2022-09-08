f80b7044bb2d7e1e07ff42b1d86e12926c653fb7
Bello Emmanuel <nathemmanuel.03@gmail.com>
2022-09-08 22:51:14 +0100

    Add task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for program
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
