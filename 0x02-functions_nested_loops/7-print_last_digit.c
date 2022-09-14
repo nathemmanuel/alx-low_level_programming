5f687e274f9710d8b2b57c771f417b515dd4c984
Bello Emmanuel <nathemmanuel.03@gmail.com>
2022-09-14 04:08:56 +0100

    Make allowance for negative numbers

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,14 @@
 
 int print_last_digit(int n)
 {
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	if (n < 0)
+	{
+		_putchar(((n % 10) * -1) + '0');
+		return ((n % 10) * -1);
+	}
+	else
+	{
+		_putchar((n % 10) + '0');
+		return (n % 10);
+	}
 }
