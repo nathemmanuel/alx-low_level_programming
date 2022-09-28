1fd0d584897c880a8a173ab009b318ee94998433
Bello Emmanuel <nathemmanuel.03@gmail.com>
2022-09-28 01:23:16 +0100

    Add code and header files

---
+++ 0x07-pointers_array_strings/0-memset.c
@@ -0,0 +1,28 @@
+# include "main,h"
+
+/**
+ * _memset - fills the first n bytes of the memory area
+ * pointed to by s with a constant byte b
+ * @s: The pointer that holds the address of the array to
+ * be manipulated
+ * @b: The constant byte
+ * Return: The pointer to the memory area s
+ */
+
+/*
+ * So we can say that s is an array, and what we're trying to do
+ * is fill the first n elements of that array with a constant byte b
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	char **p;
+
+	p = &s;
+	for (i = 0; i <= n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (p);
+}
