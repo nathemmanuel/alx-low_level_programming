a731083b6a573e06a887919d4dc3ce04b0886847
Bello Emmanuel <nathemmanuel.03@gmail.com>
2022-09-29 01:54:42 +0100

    Debug code

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -13,12 +13,11 @@ void _puts_recursion(char *s)
 
 	a = 0;
 	if (*(s + a) == '\0')
-		return;
-	else
 	{
-		_putchar(*(s + a));
-		a++;
-		_puts_recursion(s + a);
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(*(s + a));
+	a++;
+	_puts_recursion(s + a);
 }
