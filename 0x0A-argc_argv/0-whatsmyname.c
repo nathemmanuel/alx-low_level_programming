4bc60312d9c887b70dc05bfb17b06de9f33ef19d
Bello Emmanuel <nathemmanuel.03@gmail.com>
2022-10-04 18:18:59 +0100

    Add code, readme and header file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name of the executable file
+ * @argc: Number count of the arguments used during compilation
+ * @argv: Pointer to an array of strings
+ *
+ * Return: 0(success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+# Arguments to main
+This repo contains code that answers tasks relating to arguments to main (argc and argv).
