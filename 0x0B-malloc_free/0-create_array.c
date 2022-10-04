0ec299d4f2016e8561e8101561707bd82c0f808c
Bello Emmanuel <nathemmanuel.03@gmail.com>
2022-10-04 01:32:01 +0100

    Add code, header file and readme

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars (a string)
+ * and initialzes it with a specific char
+ * @size: Size of the space to be allocated
+ * @c: The char it will be initialised with
+ *
+ * Return: A pointer to the array or NULL (fail)
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *strng;
+
+	if (size  == 0)
+		return NULL;
+	if (strng == NULL)
+		return NULL;
+	strng = malloc(size * sizeof(char));
+	strng[0] = 'c';
+	return (strng);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+# 0x0B-malloc_free
+This repo contains code for tasks from the ALX SE programme on Dynamic Memory Allocation
+## Task 0
