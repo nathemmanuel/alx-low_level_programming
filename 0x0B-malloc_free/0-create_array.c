ee139e4d87f19aa1ad25a02c335ca2cf5efda24f
nathemmanuel <nathemmanuel@gmail.com>
2022-10-04 18:50:25 -0700

    Initialise with char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,12 +15,12 @@ char *create_array(unsigned int size, char c)
 {
 	char *strng;
 
+	if (size == 0)
+		return (NULL);
 	strng = malloc(size * sizeof(char));
-	strng[0] = c;
-	if (size  == 0)
-		return NULL;
 	if (strng == NULL)
-		return NULL;
+		return (NULL);
+	strng[0] = c;
 	free(strng);
 	return (strng);
 }
