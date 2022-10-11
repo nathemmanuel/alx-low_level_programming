df16de0b5a2488fc53af9387945efc7724965c87
Bello Emmanuel <nathemmanuel.03@gmail.com>
2022-10-11 14:29:00 +0100

    Create struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+# Structs and typedef
+User-defined datatypes and giving primitive data types a custom name -- that's what I'm doing.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+/**
+ * dog - Takes the name, age and owner of the dog
+ * @name: The name of the dog
+ * @age: The dog's age
+ * @owner: The owner of the dog
+ *
+ * Description: This takes the basic information of the dog, nothing serious
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+	/*
+	 * 'char *' just means it receives the address of the first element
+	 * of the array
+	 */
+}
