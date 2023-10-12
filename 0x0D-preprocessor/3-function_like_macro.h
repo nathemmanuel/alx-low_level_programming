5455aa1e5945ed1c9e77efacf8f4a85a49ced807
nathemmanuel <nathemmanuel.03@gmail.com>
2023-10-12 15:10:01 +0100

    Debug: Added a parentheses to the negative condition
    Macros expand without evaluating so when the argument was  it expanded and ended up with  which is the negative condition. Hence, the bug.

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,5 @@
 #ifndef ABSOLUTE_H
 #define ABSOLUTE_H
-#define ABS(x) (((x) >= 0) ? (x) : (-x))
+#define ABS(x) (((x) >= 0) ? (x) : (-(x)))
+/* keep in mind that macros expand without evaluating first */
 #endif
