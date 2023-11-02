f145bb9b70b991a666d9c3932a2daf51ab407bc5
nathemmanuel <nathemmanuel.03@gmail.com>
2023-11-02 01:39:02 +0100

    Testing the concepts

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars and initializes
+ * it with a specific char
+ * @size: This represents the size of the array
+ * @c: This is the initializing character
+ *
+ * Return: NULL if size is 0, returns a pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+		return (NULL);
+
+	char *some_array;
+	some_array = malloc(sizeof(size));
+	if (some_array == NULL)
+		return (NULL);
+	some_array[0] = c;
+
+	return (some_array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+# Dynamic Memory Allocation
+`Malloc`, I `calloc` `realloc` the free object. Hehe, I know that makes no sense, but in this reposiitory I worked with functions in the standard library that heloed me to allocate memory dynamically. 
