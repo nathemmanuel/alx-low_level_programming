03b86b81cd849255303f9918278b6ee367adf674
nathemmanuel <nathemmanuel.03@gmail.com>
2023-11-16 00:30:09 +0100

    Add: Function c file and header file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,27 +3,19 @@
 /**
  * _puts_recursion - prints a string recursively,
  * followed by a new line
- * @s: The string it prints recursively
+ * @s: The pointer to the first element
  *
  * Return: Void
  */
 
 void _puts_recursion(char *s)
 {
-	/* int n;
-	 * 
-	 * n = 0;
-	 *
-	 * Instead of adding n which is a move used to keep a loop
-	 * going, I can just add 1 because it adds 1 every time
-	 * (kinda like ++ for a loop)
+	/* for a recursive operation, I was told
+	 * to always include an exit operation, so
+	 * what is that exit condition now?
+	 * When s[] hits '\0'
 	 */
-
-	/* if (*(s + n) == '\0')  
-	 * I was referring to the string with this bit of code, not the pointer
-	 * which was the argument required
-	 */
-	if (*s == '\0')
+	if (*(s) == '\0')
 	{
 		_putchar('\n');
 		return;
