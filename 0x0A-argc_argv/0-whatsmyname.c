a0542bf69d04ea78874028dfe81c35bbab9b0a74
nathemmanuel <nathemmanuel.03@gmail.com>
2023-11-20 14:46:39 +0100

    Add: Code, readme and header files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - Write a program that prints its name,
+ * followed by a new line
+ * @argc: A count of the arguments supplied during runtime\
+ * @argv: A pointer to the array of strings
+ *
+ * Return: 0 (indicating success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	exit(EXIT_SUCCESS);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+# Arguments to main
+The general objectives for this project was to teach me to use arguments passed to my programs, to know the other prototype of main and to know when to use which and how to use `__attribute__((unused))` or `(void)` to compile functions with unused variables or parameters. 
